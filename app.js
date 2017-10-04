c648409c33ad48a8e59b8f1d70ec1e15cade77c3
Temuujin92 <gmrsnvrdie@gmail.com>
2017-10-04 11:32:54 +0800

    fixes

--- app.js
+++ app.js
@@ -279,11 +279,10 @@ function receivedMessage(event) {
             startMessage(senderID); 
             break;     
       case 'Салбар:
+     case 'salbar':
             salbarMessage(senderID); 
             break;          
-       case 'salbar:
-            salbarMessage(senderID); 
-            break;        
+           
         case 'мэдээ':
             sendNewsMessage(senderID); 
             break; 
@@ -744,7 +743,133 @@ function salbarMessage(recipientId) {
   };  
   callSendAPI(messageData);
 }
+function CUSTOM_salbarub(recipientId) {
+     var messageData = {
+        recipient: {
+          id: recipientId
+        },
+        message: {
+          attachment: {
+            type: "template",
+            payload: {
+              template_type: "generic",
+              elements: [{
+                title: "Төв Плаза",
+                subtitle: "*СБДүүрэг, Чингисийн өргөн чөлөө-9, Скайтел Плаза
+                           * 7611-2000 
+                           * Даваа-Баасан: 08:30-20:30 Бямба,Ням: 10:00-19:00",
+                item_url: `https://www.skytel.mn/content/branches/Ulaanbaatar`,               
+                image_url: 'https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png',
+                buttons: [{
+                   type: "postback",
+                  title: "Буцах",
+                  payload: `CUSTOM_Back`,
+                }],
+              },{
+                title: "Залуус салбар",
+                subtitle: "*СБДүүрэг, Мэдээлэл технологийн үндэсний паркын 1 давхар 
+* 7611-2005
+* Даваа-Баасан: 09:00-20:00 Бямба,Ням: 10:00-20:00
+",
+                item_url: `https://www.skytel.mn/content/branches/Ulaanbaatar`,               
+                image_url: 'https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png',
+               buttons: [{
+                   type: "postback",
+                  title: "Буцах",
+                  payload: `CUSTOM_Back`,
+                }],
+              },{
+                title: "ӨНӨР салбар",
+                subtitle: "*СХДүүрэг, 1-р хороолол, Голомт банкны байр
+* 7611-2002
+* Даваа-Баасан: 09:00-20:00 Бямба,Ням: 10:00-18:00",
+                item_url: `https://www.skytel.mn/content/branches/Ulaanbaatar`,               
+                image_url: 'https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png',
+               buttons: [{
+                   type: "postback",
+                  title: "Буцах",
+                  payload: `CUSTOM_Back`,
+                }],
+              },{
+                title: "Скайтел үйлчилгээний төв",
+                subtitle: "*ЧДүүрэг, 3-р хороо, Тэнгис кино театрийн замын урд талд 
+* 7611-2001
+* Даваа-Баасан: 08:30-20:00 Бямба,Ням: 10:00-19:00",
+                item_url: `https://www.skytel.mn/content/branches/Ulaanbaatar`,               
+                image_url: 'https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png',
+               buttons: [{
+                   type: "postback",
+                  title: "Буцах",
+                  payload: `CUSTOM_Back`,
+                }],
+              },{
+                title: "Улсын их дэлгүүр салбар",
+                subtitle: "*ЧДүүрэг, 3-р хороо, Энх тайвны өргөн чөлөө-25 Улсын их дэлгүүр 5-р давхар
+* 7611-2010
+* Даваа-Баасан: 09:00-21:30 Бямба,Ням: 09:30-21:30",
+                item_url: `https://www.skytel.mn/content/branches/Ulaanbaatar`,               
+                image_url: 'https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png',
+               buttons: [{
+                   type: "postback",
+                  title: "Буцах",
+                  payload: `CUSTOM_Back`,
+                }],
+              },{
+                title: "ОРГИЛ салбар",
+                subtitle: "ХУДүүрэг, Зайсан, Оргил худалдааны төвийн 1 давхар
+* 7611-2009
+* Даваа-Баасан: 10:00-20:00 Бямба,Ням: 10:00-19:00",
+                item_url: `https://www.skytel.mn/content/branches/Ulaanbaatar`,               
+                image_url: 'https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png',
+               buttons: [{
+                   type: "postback",
+                  title: "Буцах",
+                  payload: `CUSTOM_Back`,
+                }],
+              },{
+                title: "САНСАР салбар",
+                subtitle: "БЗДүүрэг, 15 хороолол, Сансарын Начин заан ломбардын байранд /Сансарын тунелийн эсрэг талд/ 
+* 7611-2006
+* Даваа-Баасан: 09:00-19:00 Бямба,Ням: 10:00-18:00",
+                item_url: `https://www.skytel.mn/content/branches/Ulaanbaatar`,               
+                image_url: 'https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png',
+               buttons: [{
+                   type: "postback",
+                  title: "Буцах",
+                  payload: `CUSTOM_Back`,
+                }],
+              },{
+                title: "Имарт",
+                subtitle: "*Баянзүрх дүүрэг, Технологийн гудамж, Имарт Чингис салбар 2 давхар
+* 7611-2008
+* Даваа-Ням: 10:00-20:00",
+                item_url: `https://www.skytel.mn/content/branches/Ulaanbaatar`,               
+                image_url: 'https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png',
+               buttons: [{
+                   type: "postback",
+                  title: "Буцах",
+                  payload: `CUSTOM_Back`,
+                }],
+              },{
+                title: "Налайх салбар",
+                subtitle: "*Налайх дүүрэг, 2-р хороо, МХС ТӨК байр, 1-р давхар, Налайх Скайтел салбар
+* 7611-2003
+* Даваа-Баасан: 09:00-19:00 Бямба: 11:00-16:00 Ням:Амарна",
+                item_url: `https://www.skytel.mn/content/branches/Ulaanbaatar`,               
+                image_url: 'https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png',
+               buttons: [{
+                   type: "postback",
+                  title: "Буцах",
+                  payload: `CUSTOM_Back`,
+                }],
+              },]
+            }
+          }
+        }
+      };  
 
+      callSendAPI(messageData);
+  };
               
 
 
