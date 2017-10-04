7c6dc3cc7dbb5ef787846d5b82ba36cd1e38e9d0
Temuujin92 <gmrsnvrdie@gmail.com>
2017-10-04 14:25:27 +0800

    fixeal

--- app.js
+++ app.js
@@ -759,51 +759,59 @@ function CUSTOM_salbarub(recipientId) {
               template_type: "generic",
               elements: [{
                 title: "Төв плаза",
-                subtitle: "*СБДүүрэг, Чингисийн өргөн чөлөө-9, Скайтел Плаза
-* 7611-2000 
-* Даваа-Баасан: 08:30-20:30 Бямба,Ням: 10:00-19:00",
+                subtitle: "aaa",
                 item_url: `https://www.skytel.mn/content/${detail[0].id}/view`,               
                 image_url: `https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png`,
                 buttons: [{
+                  type: "web_url",
+                  url: `https://www.skytel.mn/content/${detail[0].id}/view`,
+                  title: "Мэдээг унших"
+                }, {
                   type: "postback",
-                  title: "Буцах",
+                  title: "Тойм унших",
                   payload: `CUSTOM_NEWS_0`,
                 }],
               },{
-                title: "Төв плаза",
-                subtitle: "*СБДүүрэг, Чингисийн өргөн чөлөө-9, Скайтел Плаза
-* 7611-2000 
-* Даваа-Баасан: 08:30-20:30 Бямба,Ням: 10:00-19:00",
-                item_url: `https://www.skytel.mn/content/${detail[0].id}/view`,               
-                image_url: `https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png`,
+                title: detail[1].title,
+                subtitle: stringUtils.getSubWords(detail[1].intro, charLimitTitle),
+                item_url: `https://www.skytel.mn/content/${detail[1].id}/view`,               
+                image_url: detail[1].image,
                 buttons: [{
+                  type: "web_url",
+                  url: `https://www.skytel.mn/content/${detail[1].id}/view`,
+                  title: "Мэдээг унших"
+                }, {
                   type: "postback",
-                  title: "Буцах",
-                  payload: `CUSTOM_NEWS_0`,
+                  title: "Тойм унших",
+                  payload: `CUSTOM_NEWS_1`,
                 }],
               },{
-                 title: "Төв плаза",
-                subtitle: "*СБДүүрэг, Чингисийн өргөн чөлөө-9, Скайтел Плаза
-* 7611-2000 
-* Даваа-Баасан: 08:30-20:30 Бямба,Ням: 10:00-19:00",
-                item_url: `https://www.skytel.mn/content/${detail[0].id}/view`,               
-                image_url: `https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png`,
+                title: detail[2].title,
+                subtitle: stringUtils.getSubWords(detail[2].intro, charLimitTitle),
+                item_url: `https://www.skytel.mn/content/${detail[2].id}/view`,               
+                image_url: detail[2].image,
                 buttons: [{
+                  type: "web_url",
+                  url: `https://www.skytel.mn/content/${detail[2].id}/view`,
+                  title: "Мэдээг унших"
+                }, {
                   type: "postback",
-                  title: "Буцах",
-                  payload: `CUSTOM_NEWS_0`,
+                  title: "Тойм унших",
+                  payload: `CUSTOM_NEWS_2`,
                 }],
               },{
-                 title: "Төв плаза",
-                subtitle: "*СБДүүрэг, Чингисийн өргөн чөлөө-9, Скайтел Плаза
-* 7611-2000 
-* Даваа-Баасан: 08:30-20:30 Бямба,Ням: 10:00-19:00",
-                item_url: `https://www.skytel.mn/content/${detail[0].id}/view`,               
-                image_url: `https://www.skytel.mn/uploads/news/4a3aa5931d8b21ef59e1e2b27555fe2384445c82.png`,
+                title: detail[3].title,
+                subtitle: stringUtils.getSubWords(detail[3].intro, charLimitTitle),
+                item_url: `https://www.skytel.mn/content/${detail[3].id}/view`,               
+                image_url: detail[3].image,
                 buttons: [{
+                  type: "web_url",
+                  url: `https://www.skytel.mn/content/${detail[3].id}/view`,
+                  title: "Мэдээг унших"
+                }, {
                   type: "postback",
-                  title: "Буцах",
-                  payload: `CUSTOM_NEWS_0`,
+                  title: "Тойм унших",
+                  payload: `CUSTOM_NEWS_3`,
                 }],
               }]
             }
